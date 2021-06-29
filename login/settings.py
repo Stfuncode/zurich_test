7d1ac46f074320307a1b89b9a8847b0bc122bf26
Jason Chao <jason.chao@infrontconsulting.com.my>
2021-06-29 11:18:16 +0800

    site change to 0

--- login/settings.py
+++ login/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
     'user_accounts', #local account
  
     "crispy_forms",
-    'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+    # 'allauth',
+    # 'allauth.account',
+    # 'allauth.socialaccount',
+    # 'allauth.socialaccount.providers.google',
     # 'rest_framework',
 ]
 
@@ -124,19 +124,19 @@ tenant_id = 'c326d7f8-e4e3-4675-9b28-27d41a70b337'
 
 
 
-SOCIALACCOUNT_PROVIDERS = {
-    'google': {
-        'SCOPE': [
-            'profile',
-            'email',
-        ],
-        'AUTH_PARAMS': {
-            'access_type': 'online',
-        }
-    }
-}
+# SOCIALACCOUNT_PROVIDERS = {
+#     'google': {
+#         'SCOPE': [
+#             'profile',
+#             'email',
+#         ],
+#         'AUTH_PARAMS': {
+#             'access_type': 'online',
+#         }
+#     }
+# }
 
-SITE_ID = 1
+# SITE_ID = 1
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
